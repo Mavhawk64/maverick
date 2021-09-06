b0f642828599d9cfef5543d9fda516b1a265d56a
Mavhawk64 <mavberk@gmail.com>
2021-09-05 20:55:26 -0500

    Updated the favicon, the wording, and included Tenaska Internship

---
+++ src/Components/RepositoriesList.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import axios from 'axios';
+
+export default class RepositoriesList extends React.Component {
+  state = {
+    repositories: [],
+  };
+
+  componentDidMount() {
+    axios.get(`https://api.github.com/users/mavhawk64`).then(res => {
+      //console.log(res);
+      this.setState({repositories: res.data});
+    });
+  }
+
+  render() {
+    return (<>
+      <i className="bi bi-laptop"></i>
+      <h2 className="mavericks-h2">{this.state.repositories.public_repos}</h2>
+      <p><strong>Repositories</strong> uploaded to GitHub</p>
+      <p style={{fontSize:8+'px'}}>Number fetched using Axios with ReactJS</p>
+      </>);
+  }
+}
+ 
---
+++ src/FollowersApp.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import FollowersList from './Components/FollowersList';
+const FollowersApp = () => {
+  return (<FollowersList/>);
+}
+export default FollowersApp;
+ 
